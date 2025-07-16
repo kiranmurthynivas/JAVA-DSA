473609acff32906a35af964a458174422b1c0604
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-16 18:14:05 +0530

    Max area in histogram

--- StackB.java
+++ StackB.java
@@ -55,10 +55,52 @@ public class StackB {
         return false;
     }
 
+    public static void maxAreaInHistogram(int[] arr){
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=arr.length-1; i>=0; i--){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+
+            if( s.isEmpty()){
+                nsr[i] = arr.length;
+            }else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+        
+         s = new Stack<>();
+
+        for(int i=0; i<arr.length; i++){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+
+            if( s.isEmpty()){
+                nsl[i] = -1;
+            }else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        for(int i=0; i<arr.length; i++){
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+        System.out.println("Maximun area in histogram is "+ maxArea);
+    }
 
     public static void main(String[] args) {
-      String str1 = "((a+b))";
-      String str2 = "(a-b)";
-      System.out.println(isDuplicate(str2));
+      int arr[] = {2,1,5,6,2,3};
+      maxAreaInHistogram(arr);
     }
 }
