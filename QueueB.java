8cfcc299e271178bac2b8e82aa501b3156558c1b
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-17 13:35:37 +0530

    Stack and Queue implementation using deque

--- QueueB.java
+++ QueueB.java
@@ -1,25 +1,57 @@
-
-import java.util.Deque;
+import java.util.*;
 import java.util.LinkedList;
 
-
-
 public class QueueB {
-  
-    public static void main(String[] args) {
+    static class Stack {
         Deque<Integer> deque = new LinkedList<>();
-        deque.addFirst(1);
-        deque.addFirst(2);
-        System.out.println(deque);
-        deque.removeFirst();
-        System.out.println(deque);
-        deque.addLast(3);
-        deque.addLast(4);
-        System.out.println(deque);
-        deque.removeLast();
-        System.out.println(deque);
-        System.out.println("first ele = "+ deque.getFirst());
-        System.out.println("last ele = "+ deque.getLast());
+
+        public void push(int data) {
+            deque.addLast(data);
+        }
+
+        public int pop(){
+            return deque.removeLast();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+
+    static  class Queue {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void add(int data) {
+            deque.addLast(data);
+        }
+
+        public int remove(){
+            return deque.removeFirst();
+        }
+
+        public int peek() {
+            return deque.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+    //    Stack s = new Stack();
+    //    s.push(1);
+    //    s.push(2);
+    //    s.push(3);
+    //    System.out.println("peek = "+s.peek());
+    //    System.out.println(s.pop());
+    //    System.out.println(s.pop());
+    //    System.out.println(s.pop());
+
+       Queue q = new Queue();
+
+       q.add(1);
+       q.add(2);
+       q.add(3);
+       System.out.println("peek ="+q.peek());
+       System.out.println(q.remove());
+       System.out.println(q.remove());
+       System.out.println(q.remove());
     }
     
 }
