89ddf6d79b0a366c670aad417a7c5bbedb1b017c
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-17 19:14:11 +0530

    Greedy fractional knapsack

--- GreedyAlgo.java
+++ GreedyAlgo.java
@@ -1,36 +1,33 @@
-import java.util.*;
+
+import  java.util.*;
 
 public class GreedyAlgo {
     public static void main(String[] args) {
-        int start[] = {1,3,0,5,8,5};
-        int end[] = {2,4,6,7,9,9};
+      int val[] = {60, 100, 120};
+      int weigth[] = {10, 20, 30};
+      int W = 50;
 
-        int activities[][] = new int[start.length][3];
-        for(int i=0; i<start.length; i++){
-            activities[i][0] = i;
-            activities[i][1] = start[i];
-            activities[i][2] = end[i];
+      double ratio[][] = new double[val.length][2];
+      for(int i =0; i<val.length; i++) {
+        ratio[i][0] = i;
+        ratio[i][1] = val[i]/(double)weigth[i];
+      }
+
+      Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+      int capacity = W;
+      int finalVal = 0;
+      for(int i=ratio.length-1; i>=0; i-- ) {
+        int idx = (int) ratio[i][0];
+        if(capacity >= weigth[idx]){
+            finalVal += val[idx];
+            capacity -= weigth[idx];
+        } else {
+            finalVal += (ratio[i][1] * capacity);
+            capacity = 0;
+            break;
         }
-        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
-
-        int maxAct = 0;
-
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        maxAct = 1;
-        ans.add(activities[0][0]);
-        int lastEnd = activities[0][2];
-        for(int i=1; i<end.length; i++) {
-            if(activities[i][1] >= lastEnd) {
-                maxAct++;
-                ans.add(activities[i][0]);
-                lastEnd = activities[i][2];
-            }
-        }
-
-        System.out.println("max activities = " + maxAct);
-        for(int i=0; i<ans.size(); i++) {
-            System.out.print("A"+ans.get(i)+" ");
-        }
+      }
+      System.out.println("Final value = "+finalVal);
     }
 }
