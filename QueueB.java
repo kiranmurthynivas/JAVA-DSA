de3ee0cfb0b7753250f5ab54c1196c27da709c82
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-17 08:13:44 +0530

    Queue implemtaion using linkedList

--- QueueB.java
+++ QueueB.java
@@ -1,37 +1,32 @@
 public class QueueB {
-    static class Queue{
-        static int arr[];
-        static int size;
-        static int rear;
-        static int front;
+   
+    static class Node{
+        int data;
+        Node next;
 
-        public Queue(int n) {
-            arr = new int[n];
-            size = n;
-            rear = -1;
-            front = -1;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
+    }
+   
+    static class Queue{
+        static Node head=null;
+        static Node tail=null;
 
         public static boolean isEmpty(){
-            return rear == -1 && front == -1;
+            return head == null && tail == null;
         }
 
-        public static boolean isFull(){
-            return (rear+1) % size == front;
-        }
 
         public static void add(int data){
-            if(isFull()){
-                System.out.println("Queue is full");
+            Node newNode = new Node(data);
+            if(head == null) {
+                head = tail = newNode;
                 return;
             }
-
-            if(front == -1){
-                front = 0;
-            }
-
-            rear = (rear+1) % size;
-            arr[rear] = data;
+            tail.next = newNode;
+            tail = newNode;
         }
 
         public static int remove(){
@@ -39,15 +34,15 @@ public class QueueB {
                 System.out.println("Empty Queue");
                 return -1;
             }
-            int result = arr[front];
+            int front = head.data;
 
-            if(rear == front) {
-                rear = front = -1;
-            }else{
-                front = (front + 1)%size;
+            if(tail == head) {
+                tail = head = null;
+            }else {
+                head = head.next;
             }
         
-            return result;
+            return front;
         }
 
         public static int peek(){
@@ -56,13 +51,13 @@ public class QueueB {
                 return -1;
             }
 
-            return arr[front];
+            return head.data;
         }
         
     }
 
     public static void main(String[] args) {
-        Queue q = new Queue(5);
+        Queue q = new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
