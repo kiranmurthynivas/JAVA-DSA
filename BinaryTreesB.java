458896a8a0b7aa051d17019e01c5b1ce37f36658
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-25 12:10:18 +0530

    Binary tree traversals inorder

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -36,11 +36,24 @@ public class BinaryTreesB {
         preorder(root.right);
     }
 
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+  
     public static void main(String[] args) {
-        int nodes [] = {1, 2, 4, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes [] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         
-        preorder(root);
+        // preorder(root);
+        inorder(root);
     }
 }
