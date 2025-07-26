5aeb00e1660ca7aee7775ac3b5b9ebbb0d9ee296
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-26 17:49:17 +0530

    Binary tree top view

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -159,6 +159,18 @@ public class BinaryTreesB {
         System.out.println("");
     }
 
+    public static void kLevel( Node root , int level,int  k) {
+        if ( root == null ) {
+            return;
+        }
+
+        if ( level == k ) {
+            System.out.print(root.data+" ");
+            return;
+        }
+        kLevel(root.left, level+1, k);
+        kLevel(root.right, level+1, k);
+    }
     public static void main(String[] args) {
        Node root = new Node(1);
        root.left = new Node(2);
@@ -168,6 +180,7 @@ public class BinaryTreesB {
        root.right.left = new Node(6);
        root.right.right = new Node(7);
 
-       topView(root);
+       int k = 2;
+       kLevel(root, 1, k);
     }
 }
