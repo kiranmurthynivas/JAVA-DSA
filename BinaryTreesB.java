f541072d26f71c31d881053281e5776353365e66
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-26 11:52:31 +0530

    Binary tree subtree in a tree

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -75,18 +75,56 @@ public class BinaryTreesB {
 
         return new Info(dia, ht);
     }
+
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if(node == null && subRoot == null ) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data ) {
+            return false;
+        }
+
+        if(!isIdentical(node.left, subRoot.left)){
+            return false;
+        }
+
+        if(!isIdentical(node.right, subRoot.right)){
+            return false;
+        }
+
+        return true;
+    }
+    
+
+   
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (isIdentical(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
    
 
     public static void main(String[] args) {
        Node root = new Node(1);
        root.left = new Node(2);
-       root.right = new Node(3);
+       root.right = new Node(3);               
        root.left.left = new Node(4);
        root.left.right = new Node(5);
        root.right.left = new Node(6);
        root.right.right = new Node(7);
 
-       System.out.println(diameter2(root).dia);
+
+       Node subRoot = new Node(2);
+       subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+
+       System.out.println(isSubtree(root, subRoot));
        
     }
 }
