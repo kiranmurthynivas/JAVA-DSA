32beab446f9eee29b7c44e6007c865cb33f18a03
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-28 09:27:29 +0530

    Binary tree min distance between nodes

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -232,6 +232,35 @@ public static Node lca2(Node root, int n1, int n2) {
     return root;
 }
 
+public static int lcaDist(Node root, int n){
+    if(root == null ) {
+        return -1;
+    }
+
+    if(root.data == n) {
+        return 0;
+    }
+
+    int leftDist = lcaDist(root.left, n);
+    int rigthDist = lcaDist(root.right, n);
+
+    if(leftDist == -1 && rigthDist == -1 ) {
+        return -1;
+    } else if (leftDist == -1)  {
+        return rigthDist+1;
+    } else {
+        return leftDist+1;
+    }
+}
+
+public static int minDist(Node root, int n1, int n2 ) {
+    Node lca = lca2(root, n1, n2);
+    int dist1= lcaDist(lca,n1);
+    int dist2= lcaDist(lca,n2);
+
+    return dist1 + dist2;
+}
+
 
     public static void main(String[] args) {
        Node root = new Node(1);
@@ -243,6 +272,6 @@ public static Node lca2(Node root, int n1, int n2) {
        root.right.right = new Node(7);
 
        int n1 = 4, n2 = 5;
-       System.out.println(lca2(root, n1, n2).data);
+       System.out.println(minDist(root, n1, n2));
     }
 }
