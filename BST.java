09a238dfd0149b715e4216f388e4e9aed9414050
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-29 10:31:57 +0530

    BST printInRange

--- BST.java
+++ BST.java
@@ -50,7 +50,7 @@ public class BST {
         }
     } 
 
-     public static Node delete(Node root, int val) {
+    public static Node delete(Node root, int val) {
         if (root == null) return null;
 
         if (root.data < val) {
@@ -86,6 +86,22 @@ public class BST {
         return root;
     }
 
+    public static void printInRange(Node root , int k1, int k2) {
+        if(root == null) {
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        }
+        else if(root.data <= k1 ) {
+            printInRange(root.right, k1, k2);
+        } 
+        else {
+            printInRange(root.left, k1, k2);
+        }
+    }
 
     public static void main(String[] args) {
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
@@ -95,9 +111,9 @@ public class BST {
             root = insert(root, values[i]);
         }
         
-        root = delete(root, 5);
+        inorder(root);
         System.out.println();
 
-        inorder(root);
+        printInRange(root, 1, 5);
     }
 }
