e47aaa41e8ea2613d34e60005dc436d09fead4de
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-07-29 09:29:15 +0530

    BST build

--- BST.java
+++ BST.java
@@ -1,3 +1,45 @@
 public class BST {
-    
+    static class Node {
+        int data;
+        Node left;
+        Node rigth;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root , int val  ) {
+        if(root == null ) {
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val ) {
+            root.left = insert(root.left, val);
+        } else {
+            root.rigth = insert(root.rigth, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root ) {
+        if(root == null ) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.rigth);
+    }
+    public static void main(String[] args) {
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
+
+        for(int i=0; i<values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+    }
 }
