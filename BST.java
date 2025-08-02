3c0c9efc5a6cd636157899224afcddb65dfcd57d
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-08-02 10:59:56 +0530

    BST merge two BST

--- BST.java
+++ BST.java
@@ -145,44 +145,7 @@ public class BST {
 
         return root;
     }
-
-    public static void predorder(Node root) {
-        if( root ==  null ) {
-            return;
-        }
-        System.out.print(root.data+" ");
-        predorder(root.left);
-        predorder(root.right);
-    } 
-
-    public static void getInorder(Node root , ArrayList<Integer> inorder) {
-        if(root == null ) {
-            return;
-        }
-        getInorder(root.left, inorder);
-        inorder.add(root.data);
-        getInorder(root.right,inorder);
-    }
-
-     public static Node  createBST(ArrayList<Integer> inorder, int st, int end ) {
-        if(st>end) {
-            return null;
-        }    
-        Integer mid = (st+end) / 2;
-        Node root = new Node(inorder.get(mid));
-        root.left = createBST(inorder, st, mid-1);
-        root.right = createBST(inorder, mid+1, end);
-        return root; 
-    }
-
-    public static Node balanceBST(Node root) {
-        ArrayList<Integer> inorder = new ArrayList<>();
-        getInorder(root, inorder);
-
-        root = createBST(inorder, 0, inorder.size()-1);
-        return root;
-    }
-
+    
     static class Info {
         boolean isBST;
         int size;
@@ -222,22 +185,80 @@ public class BST {
         return new Info(false, size, min, max);
     }
 
+     public static void predorder(Node root) {
+        if( root ==  null ) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        predorder(root.left);
+        predorder(root.right);
+    } 
+   
+    public static Node  createBST(ArrayList<Integer> arr, int st, int end ) {
+        if(st>end) {
+            return null;
+        }    
+        Integer mid = (st+end) / 2;
+        Node root = new Node(arr.get(mid));
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+        return root; 
+    }
+     
+    public static void getInorder(Node root , ArrayList<Integer> arr) {
+        if(root == null ) {
+            return;
+        }
+        getInorder(root.left, arr);
+        arr.add(root.data);
+        getInorder(root.right,arr);
+    }
+    
+    public static Node mergeBSTs(Node root1, Node root2) {
+        
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        getInorder(root1, arr1);
+
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        getInorder(root2, arr2);
+
+        int i=0, j=0;
+        ArrayList<Integer> finalArr = new ArrayList<>();
+        while(i<arr1.size() && j<arr2.size()){
+            if(arr1.get(i) <= arr2.get(j)) {
+                finalArr.add(arr1.get(i));
+                i++;
+            } else {
+                finalArr.add(arr2.get(j));
+                j++;
+            }
+        }
+
+        while(i<arr1.size()) {
+            finalArr.add(arr1.get(i));
+            i++;
+        }
+
+        while(j<arr2.size()) {
+            finalArr.add(arr2.get(j));
+            j++;
+        }
+
+       return  createBST(finalArr, 0, finalArr.size()-1);
+    }
+
     public static void main(String[] args) {
       
-        Node root = new Node(50);
-        root.left = new Node(30);
-        root.left.left = new Node(5);
-        root.left.right = new Node(20);
+        Node root1 = new Node(2);
+        root1.left = new Node(1);
+        root1.right = new Node(4);
 
-        root.right = new Node(60);
-        root.right.left = new Node(45);
-        root.right.right = new Node(70);
-        root.right.right.left = new Node(65);
-        root.right.right.right = new Node(80);
+        Node root2 = new Node(9);
+        root2.left = new Node(3);
+        root2.right = new Node(12);
 
+        Node root = mergeBSTs(root1, root2);
 
-        Info info = largestBSt(root);
-
-        System.out.println(maxBST);
+        predorder(root);
     }
 }
