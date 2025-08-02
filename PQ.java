ba5f615c9ef8e6a11cc310858485624bb5dace48
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-08-02 16:10:34 +0530

    Priority queus for objects

--- PQ.java
+++ PQ.java
@@ -1,19 +1,33 @@
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PQ {
+
+    static  class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
     
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Student> pq = new PriorityQueue<>();
 
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
 
         while(!pq.isEmpty()) {
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name+" -> "+ pq.peek().rank);
             pq.remove();
         }
     }
