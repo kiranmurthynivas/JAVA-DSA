d7a617d9fccad3243765e688cba2ce269af2b6f3
Kiran Murthy K S <kiranmurthynivas29@gmail.com>
2025-08-03 10:59:27 +0530

    Heaps nearst K cars

--- Heaps.java
+++ Heaps.java
@@ -1,49 +1,41 @@
 
+import java.util.PriorityQueue;
+
+
 
 public class Heaps {
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+        int distSq;
+        int idx;
 
-    public static void heapify(int arr[], int i, int size) {
-        int left = 2*i+1;
-            int right = 2*i+2;
-            int maxIdx = i;
-
-            if(left < size && arr[left] > arr[maxIdx]) {
-                maxIdx = left;
-            } 
-
-            if(right < size && arr[right] > arr[maxIdx]) {
-                maxIdx = right;
-            } 
-
-            if(maxIdx !=i) {
-                int temp = arr[i];
-                arr[i] = arr[maxIdx];
-                arr[maxIdx] = temp;
-                heapify(arr, maxIdx, size);
-            }
-    }
-
-    public static void  heapSort(int arr[]) {
-        int n = arr.length;
-        for(int i=n/2; i>=0; i--) {
-            heapify(arr,i, n);
+        public Point(int x, int y, int distSq, int idx) {
+            this.x = x;
+            this.y = y;
+            this.distSq = distSq;
+            this.idx = idx;
         }
 
-        for(int i=n-1; i>0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-
-            heapify(arr,0, i);
+        @Override
+        public int compareTo(Point p2) {
+            return this.distSq - p2.distSq;
         }
     }
+ 
+
     public static void main(String[] args) {
-        int arr[] = {1,2,4,5,3};
-        heapSort(arr);
+        int pts[][] = {{3, 3}, {5, -1}, {-2, 4}};
+        int k = 2;
 
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]+" ");
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for(int i=0; i<pts.length; i++ ) {
+            int distSq = pts[i][0]*pts[i][0] + pts[i][1]*pts[i][1];
+            pq.add(new Point(pts[i][0], pts[i][1], distSq,i));
         }
-        System.out.println("");
+
+        for(int i=0; i<k; i++) {
+            System.out.println("C"+pq.remove().idx);
+        }
     }
 }
